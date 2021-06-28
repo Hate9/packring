ea36fa82813813562b1c255cf1e358f5f732ac7d
Mabel Schaefer <als.hate.09@gmail.com>
2021-06-28 14:20:22 -0400

    fixed ajax.js being a symlink

--- js/ajax.js
+++ js/ajax.js
@@ -1,1 +1,40 @@
-/run/media/mabel/Terrabyteous/xampp/htdocs/lib/js/ajax.js
+/*ajaxRequest: sends a simple asynchronous ajax request and returns the response
+  parameters:
+  url: the url to which to send the request
+  formData: either the FormData object or an associative array which will be converted to a FormData object, which will be sent
+  cache: a boolean specifying whether or not to cache the requested data. if set to false, predefined request headers will be sent to prevent caching
+  headers: an associative array representing the keys and values of request headers to be sent
+*/
+function ajaxRequest(url, formData = null, cache = true, headers = null) {
+	return new Promise(resolve => {
+		let xhr = new XMLHttpRequest();
+		
+		if (!(formData instanceof FormData)) {
+			let tempFormData = new FormData();
+			for (let key in formData) {
+				tempFormData.append(key, formData[key]);
+			}
+			formData = tempFormData;
+		}
+		
+		if (!cache) {
+			xhr.setRequestHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+			xhr.setRequestHeader("Pragma", "no-cache");
+			xhr.setRequestHeader("Expires", "Fri, 01 Jan 1990 00:00:00 GMT");
+		}
+		
+		if (headers != null) {
+			for (let key in headers) {
+				xhr.setRequestHeader(key, headers[key]);
+			}
+		}
+		
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				resolve(this.responseText);
+			}
+		};
+		xhr.open(formData == null ? "GET" : "POST", url, true);
+		xhr.send(formData);
+	});
+}
