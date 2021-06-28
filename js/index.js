95ba4698c91625ace2a6fd2efa25d9a9426a0a84
Mabel Schaefer <als.hate.09@gmail.com>
2021-06-28 15:21:51 -0400

    moved to testing ajax

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 var converter = new showdown.Converter();
 
-var text = "---\nTitle: 'WitchCraft'\nAuthor: 'Hate9'\nCSS: 'witchcraft.css'\nURL: 'https://www.curseforge.com/minecraft/modpacks/witchcraft-dimensions'\n---\n# WitchCraft\n**WitchCraft** is a modpack.";
-const yamlBlockPattern = /^---[\r\n]*(.*)^---/gsm;
-const markdownBlockPattern = /\n---(.*(?!---).*)$/gs;
-var yamlText = yamlBlockPattern.exec(text)[1];
-var markdownText = markdownBlockPattern.exec(text)[1];
-console.log(jsyaml.load(yamlText));
-console.log(converter.makeHtml(markdownText));
-document.getElementById("content").innerHTML = converter.makeHtml(markdownText);
+window.onload = async function() {
+	var text = await ajaxRequest("packs/witchcraft.md");
+	const yamlBlockPattern = /^---[\r\n]*(.*)^---/gsm;
+	const markdownBlockPattern = /\n---(.*(?!---).*)$/gs;
+	var yamlText = yamlBlockPattern.exec(text)[1];
+	var markdownText = markdownBlockPattern.exec(text)[1];
+	console.log(jsyaml.load(yamlText));
+	console.log(converter.makeHtml(markdownText));
+	document.getElementById("content").innerHTML = converter.makeHtml(markdownText);
+}
