59be91e4c2dec4b744a088f66edbc811e30cf4c9
Mabel Schaefer <als.hate.09@gmail.com>
2021-06-29 00:23:35 -0400

    implemented first stage of modpack.js

--- js/modpack.js
+++ js/modpack.js
@@ -1,1 +1,15 @@
-alert(window.location.search.replace("?", "")); 
+window.onload = async function() {
+	var converter = new showdown.Converter();
+	let text = await ajaxRequest("/packs/"+window.location.search.replace("?", "")+".md");
+	let yaml = jsyaml.load(
+		/^---[\r\n]*(.*)^---/gsm.exec(
+			text
+		)[1]
+	);
+	let markdown = converter.makeHtml(
+		/\n---(.*(?!---).*)$/gs.exec(
+			text
+		)[1]
+	);
+	document.title = yaml.Title;
+}
