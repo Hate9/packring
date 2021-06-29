aac9aad01ed22dbcae9bbab2d0723e2d3a572d0b
Mabel Schaefer <als.hate.09@gmail.com>
2021-06-29 00:22:58 -0400

    removed unnecessary code from index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-var converter = new showdown.Converter();
-
 window.onload = async function() {
 	var packs = (await ajaxRequest(".packs", true, "text/plain")).split("\n");
 	for (i = 0; i < packs.length; i++) {
-		let text = await ajaxRequest("packs/"+packs[i]);
-		let yaml = jsyaml.load(/^---[\r\n]*(.*)^---/gsm.exec(text)[1]);
-		let markdown = converter.makeHtml(/\n---(.*(?!---).*)$/gs.exec(text)[1]);
+		let yaml = jsyaml.load(
+			/^---[\r\n]*(.*)^---/gsm.exec(
+				await ajaxRequest("/packs/"+packs[i])
+			)[1]
+		);
 		
 		insertDOMElement("a", "<img class=\"packimage\" src=\"res/"+yaml.Image+"\" alt=\""+yaml.Title+"\" title=\""+yaml.Title+"\"></img>", document.getElementById("packlist"))
 			.setAttribute("href", "modpack?"+packs[i].replace(/\.[^/.]+$/, ""));
